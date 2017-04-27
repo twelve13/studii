69a14c1c351011e3ec95877d54387f59f0639a8e
Caroline Chen <carolinecaroline@gmail.com>
2017-04-27 17:00:32 -0400

    read boards and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root to: "boards#index"
+
+	resources :boards do
+		resources :posts do
+			resources :notes
+		end
+	end
 end
