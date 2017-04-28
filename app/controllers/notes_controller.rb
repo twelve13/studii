f30fb6f52cd678a9afd46b3a00148bbbdba53d1e
Caroline Chen <carolinecaroline@gmail.com>
2017-04-28 12:33:19 -0400

    start cud on notes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,44 @@
+class NotesController < ApplicationController
+
+def new
+	@board = Board.find(params[:board_id])
+	@post = @board.posts.find(params[:post_id])
+	@note = Note.new
+end
+
+def create
+	@board = Board.find(params[:board_id])
+	@post = @board.posts.find(params[:post_id])
+	@note = @post.create(note_params)
+	redirect_to board_post_path(@board, @post)
+end
+
+def edit
+	@board = Board.find(params[:board_id])
+	@post = Post.find(params[:id])
+end
+
+def update
+	@board = Board.find(params[:board_id])
+	@post = @board.posts.find(params[:id])
+	@post.update(post_params)
+	redirect_to board_path(@board)
+end
+
+def destroy
+	@board = Board.find(params[:board_id])
+	@post = @board.posts.find(params[:id])
+	@post.destroy
+	redirect_to board_path(@board)
+end
+
+private
+def post_params
+	params.require(:post).permit(:title, :image, :url, :board_id)
+end
+
+def note_params
+	params.require(:note).permit(:body, :board_id, :post_id)
+end
+
+end
