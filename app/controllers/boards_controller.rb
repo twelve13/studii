507437e4097db2272e47a76cada5968a7c000e03
Caroline Chen <carolinecaroline@gmail.com>
2017-04-28 10:05:18 -0400

    full CRUD on boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,28 +8,35 @@ def show
 	@board = Board.find(params[:id])
 end
 
+def new
+	@board = Board.new
+end
 
+def create
+	@board = Board.create(board_params)
+	redirect_to root_path
+end
 
+def edit
+	@board = Board.find(params[:id])
+end
 
+def update
+	@board = Board.find(params[:id])
+	@board.update(board_params)
+	redirect_to root_path
+end
 
+def destroy
+	@board = Board.find(params[:id])
+	@board.destroy
+	redirect_to boards_path
+end
 
+private
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def board_params
+	params.require(:board).permit(:title, :image)
+end
 
 end
